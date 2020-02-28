6dd9b0c3d6e5964dd95342296f92782ad80adca9
antoinethebault <antoine.thebau@gmail.com>
2020-02-28 12:11:35 +0100

    second commit

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -3,8 +3,28 @@ package fr.declaration.variable;
 public class DeclarationApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// declaration des variables :
+		byte b = 127;
+		short s = 32767;
+		int i = 2_100_000_000;
+		long l = 2L;
+		float f = 3.4F;
+		double d = 1.79D;
+		char c = 'c';
+		boolean bool = true;
+		String randomString = "Voici le résultat d’un calcul :\n 1+5=6";
 		
+		//affichage des variables :
+		System.out.println("Affichage des valeurs :");
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(bool);
+		System.out.println(randomString);
 	}
 
 }
