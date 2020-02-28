61f69c2fa0c99dd4e0342ee9815ed5058d28be06
antoinethebault <antoine.thebau@gmail.com>
2020-02-28 13:05:30 +0100

    variablelong

--- src/fr/declaration/variablelong/DeclarationLong.java
+++ src/fr/declaration/variablelong/DeclarationLong.java
@@ -1,5 +1,16 @@
 package fr.declaration.variablelong;
 
 public class DeclarationLong {
-
+	public static void main(String[] args) {
+		
+		/*-----declaration des variables-----
+		 * 
+		on precise avec un L ou l a la fin notamment si le nombre 
+		depasse un int */
+		
+		long a = 3000L; //L optionnel
+		long b = 3000 ;
+		long c = 3000000000L; // L obligatoire (ou l)
+		
+	}
 }
