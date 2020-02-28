4d4ad01692177fcf25a62c0dca6a926ec331dd35
antoinethebault <antoine.thebau@gmail.com>
2020-02-28 16:46:52 +0100

    algorithmie recherche min

---
+++ src/fr/algorithmie/RechercheMin.java
@@ -0,0 +1,20 @@
+package fr.algorithmie;
+
+public class RechercheMin {
+
+	public static void main(String[] args) {
+		// declaration de la variable
+		int[] array = {1,15,-3,0,8,7,4,-2,28,7,-1,17,2,3,0,14,-4};
+
+
+		// Recherche le plus grand element
+		int min = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < min)
+				min = array[i];
+		}
+		System.out.println("Le plus petit element du tableau : " + min);
+
+	}
+
+}
