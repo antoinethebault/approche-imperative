2815d25796c3b4cddb67238b68cfb2679a383ae7
antoinethebault <antoine.thebau@gmail.com>
2020-02-28 16:09:30 +0100

    check

---
+++ src/fr/algorithmie/AffichageInverse.java
@@ -0,0 +1,22 @@
+package fr.algorithmie;
+
+public class AffichageInverse {
+	
+	public static void main(String[] args) {
+		//declaration de la variable
+		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+		
+		//affichage de l'ensemble des elements du tableau
+		System.out.println("affichage des elements du tableau :");
+		for (int i=0; i<array.length; i++) {
+			System.out.print(array[i]+" ");
+		}
+		
+		//affichage de l'ensemble des elements du tableau en ordre inverse
+		System.out.println("\n\naffichage des elements du tableau en ordre inverse :");
+		for (int i=array.length-1; i>=0; i--) {
+			System.out.print(array[i]+" ");
+		}
+	}
+	
+}
--- src/fr/declaration/variablelong/DeclarationLong.java
+++ src/fr/declaration/variablelong/DeclarationLong.java
@@ -1,18 +1,18 @@
 package fr.declaration.variablelong;
 
 public class DeclarationLong {
+	
 	public static void main(String[] args) {
 		
 		/*-----declaration des variables-----
 		 * 
-		on precise avec un L ou l a la fin notamment si le nombre 
+		on precise avec un L ou l a la fin obligatoirement si le nombre 
 		depasse un int (2,1M)*/
 		
-		long a = 3000L; //L optionnel
+		long a = 3000L; //L ou l optionnel
 		long b = 3000 ;
 		long c = 3000000000L; // L obligatoire (ou l)
 		
-		//
 		
 	}
 }
