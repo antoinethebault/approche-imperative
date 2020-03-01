999f0c46a426fbc793e69f4397346cb83ab358d0
antoinethebault <antoine.thebau@gmail.com>
2020-03-01 16:55:55 +0100

    algorithmie interactif plus moins

---
+++ src/fr/algorithmie/InteractifPlusMoins.java
@@ -0,0 +1,32 @@
+package fr.algorithmie;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class InteractifPlusMoins {
+
+	public static void main(String[] args) {
+		//declaration des variables
+		Scanner saisie = new Scanner(System.in);
+		int nb;
+		int essais=1;
+		
+		//choix d'un nombre aleatoire
+		int secretNb = new Random().nextInt(100)+1;
+		
+		//saisie d'un nombre jusqu'a trouver le bon
+		System.out.println("Saisissez un nombre :");
+		nb=saisie.nextInt();
+		while (nb!=secretNb) {
+			if (nb<secretNb)
+				System.out.println("Le nombre est plus grand");
+			else
+				System.out.println("Le nombre est plus petit");
+			nb=saisie.nextInt();
+			essais++;
+		}
+		System.out.println("Bravo, vous avez trouvé en "+essais+" coups");
+
+	}
+
+}
