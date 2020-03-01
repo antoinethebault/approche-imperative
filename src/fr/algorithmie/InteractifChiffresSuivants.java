1add64a6ecfbeb486b713c5ae144924ff07f6bbc
antoinethebault <antoine.thebau@gmail.com>
2020-03-01 11:45:18 +0100

    algorithmie chiffres suivants

---
+++ src/fr/algorithmie/InteractifChiffresSuivants.java
@@ -0,0 +1,21 @@
+package fr.algorithmie;
+
+import java.util.Scanner ;
+
+public class InteractifChiffresSuivants {
+
+	public static void main(String[] args) {
+		//declaration des variables
+		Scanner saisie = new Scanner(System.in) ;
+		
+		//saisie d'un nombre
+		System.out.println("Saisir un nombre : ");
+		int nb = saisie.nextInt() ;
+		
+		//affichage des 10 suivants
+		for (int i=1; i<=10; i++)
+			System.out.print((nb+i)+", ");
+
+	}
+
+}
