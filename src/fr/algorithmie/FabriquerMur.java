0d1eba48005f016fac26766c18cb6f11e6061340
antoinethebault <antoine.thebau@gmail.com>
2020-03-01 11:40:35 +0100

    algorithmie fabriquer mur

---
+++ src/fr/algorithmie/FabriquerMur.java
@@ -0,0 +1,31 @@
+package fr.algorithmie;
+
+public class FabriquerMur {
+	public static void main(String[] args) {
+		// Tests de vérification
+		verifier(3, 1, 8, true);
+		verifier(3, 1, 9, false);
+		verifier(3, 2, 10, true);
+		verifier(3, 2, 8, true);
+		verifier(3, 2, 9, false);
+		verifier(6, 1, 11, true);
+		verifier(6, 0, 11, false);
+		verifier(1, 4, 11, true);
+		verifier(0, 3, 10, true);
+		verifier(1, 4, 12, false);
+		verifier(3, 1, 7, true);
+		verifier(1, 1, 7, false);
+	}
+
+	static boolean fabriquerMur(int nbSmall, int nbBig, int longueur) {
+		boolean resultat = false; 
+		// TODO: implémenter l'algo 
+		return resultat;
+	}
+
+	private static void verifier(int nbSmall, int nbBig, int longueur, boolean b) {
+		if (!fabriquerMur(nbSmall, nbBig, longueur) == b) {
+			throw new RuntimeException("Test (" + nbSmall + ", " + nbBig + ", " + longueur + ") NON passant.");
+		}
+	}
+}
